5cc3be2a412eb60d10adb3a5b6f5d2a451f584f3
Alexandra Vacca <lexetc.claudia@outlook.com>
2021-02-28 19:39:25 +0400

    article we added new and create

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,20 @@
+class ArticlesController < ApplicationController
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @articles = Article.new(article_params)
+    if @article.save
+      redirect_to article_path(@article)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:name, :gender, :size, :price_per_month, :details)
+  end
+end
