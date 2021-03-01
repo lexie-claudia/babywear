95187774818cdf7a7d403a3edea63ecec5a2c4ec
Alexandru Iliesi <iliesialexandru@gmail.com>
2021-03-01 15:14:58 +0400

    destroy for articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,5 @@
 class ArticlesController < ApplicationController
-
+  before_action :set_article, only: [:show, :destroy]
   def index
     @articles = Article.all
   end
@@ -21,7 +21,16 @@ class ArticlesController < ApplicationController
     end
   end
 
+  def destroy
+    @article.destroy
+    redirect_to articles_path
+  end
+
   private
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
 
   def article_params
     params.require(:article).permit(:name, :gender, :size, :price_per_month, :details)
