14a31226788b65add005ed6a59346af9e46d609c
Alexandru Iliesi <iliesialexandru@gmail.com>
2021-03-01 12:20:27 +0400

    seed 10 articles

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,7 +2,7 @@ class Article < ApplicationRecord
   has_many :bookings
   belongs_to :user
   validates :name, presence: true, uniqueness: true
-  GENDER = %w(girl boy)
+  GENDER = %w(girls boys unisex)
   validates :gender, inclusion: { in: GENDER }
   SIZE = ['0-3mths', '3-6mths', '6-9mths', '9-12mths', '12-18mths', '18-24mths']
   validates :size, inclusion: { in: SIZE }
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+ p 'database cleared'
+ p "--------------------------------"
+
+Article.destroy_all
+
+10.times do
+  p Article.create(
+    name: Faker::Name.name,
+    gender: Article::GENDER.sample,
+    size: Article::SIZE.sample,
+    price_per_month: [5, 10, 15, 20].sample,
+    details: Faker::TvShows::BigBangTheory.quote,
+    user: User.first
+  )
+end
+  p "--------------------------------"
+  p '10 articles created'
