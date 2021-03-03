df19628aed3c04be13528525dc62634e24cb09ce
Alexandra Vacca <lexetc.claudia@outlook.com>
2021-03-03 17:37:40 +0400

    Edited the Navbar and created a SEED for for bookings, Also added price to be added before items is created (before_validation :set_price in booking.rb

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,17 +1,26 @@
 class Booking < ApplicationRecord
-  before_save :default_values
-
+  # Associations
   belongs_to :article
   belongs_to :user
+
+  # Validations
   validates :start_date, :message, presence: true
   STATUS = %w(pending accepted declined)
   validates :status, inclusion: { in: STATUS }
   WEEKS = [2, 4, 6, 8]
   validates :number_of_weeks, inclusion: { in: WEEKS }
 
+  # Callbacks
+  before_validation :set_status, on: :create
+  before_validation :set_price, on: :create
+
   private
 
-  def default_values
+  def set_status
     self.status = status.presence || 'pending'
+  end
+
+  def set_price
+    self.price = self.article.price_per_month * self.number_of_weeks
   end
 end
